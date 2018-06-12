19332171fecf81ede177b8858a2c40c970a74f07
vaibnak <balamanju780@gmail.com>
2018-06-12 12:41:19 +0530

    redux store created

---
+++ src/Superherolist.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Superhero extends Component{
+render(){
+
+    return <h1>Welcome to the age of superheroes</h1>
+
+
+}
+}
+
+export default Superhero
---
+++ src/storeReducer.js
@@ -0,0 +1,7 @@
+export default function(){
+
+{name = "ironman", speciality = "good technical knowledge", color= "red" }
+{name = "captain america", speciality = "serum injected superpower", color= "blue" }
+{name = "Thor", speciality = "literally god", color= "grey" }
+{name = "Hulk", speciality = "enhanced power due to gamma radiations", color= "green" }
+}
