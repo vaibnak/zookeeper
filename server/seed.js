d29445b183f9b19b3f367a4287c6c90ba3f52817
vaibnak <balamanju780@gmail.com>
2018-06-19 20:01:57 +0530

    faker.js added with lodash

---
+++ server/seed.js
@@ -0,0 +1,28 @@
+
+module.exports =  function () {
+    let faker = require('faker');
+    let _ = require('Lodash');
+    return {
+        animals: _.times(100, function (num) {
+            return {
+                id: num,
+                name: faker.name.findName(),
+                species: faker.company.catchPhrase(),
+                gender: faker.helpers.randomize(['m', 'f']),
+                age: faker.random.number(100),
+            };
+        }),
+        zookeeper: _.times(20, function(i){
+            return{
+            id: i,
+            name: {
+             firstname: faker.name.firstName(),
+             lastname: faker.name.lastName(),
+            },
+            salary: faker.random.number(30000,70000) 
+
+        }
+        })
+    
+    };
+}
